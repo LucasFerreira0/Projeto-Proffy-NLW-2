fd5520fe82c99db4a54f887417921615ed5ddf6c
lucas Ferreira <68358008+LucasFerreira0@users.noreply.github.com>
2020-08-10 22:08:26 -0300

    Add files via upload

--- public/scripts/successRedirect.js
+++ public/scripts/successRedirect.js
@@ -1,5 +1,10 @@
 const queryString = document.getElementById("queryString")
+const buttonAccess = document.getElementById("button-access")
+buttonAccess.addEventListener("click",function(){
+    location.href="/study"+queryString.value
+})
 
 setTimeout(()=>{
     location.href="/study"+queryString.value
 },2000)
+
